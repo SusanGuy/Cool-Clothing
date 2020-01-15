e677461c2bd718e25d2b68772d5d468217434983
Susan Subedi <susansubedi34@gmail.com>
2020-01-14 22:10:24 -0600

    Added Navigation Bar that links to relative pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navigation from "./components/navigation/navigation";
 import HomePage from "./containers/homepage/homepage";
 import Shop from "./containers/shop/shop";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
