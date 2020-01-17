27a77c644fad99c4a6bf61b16cad27a528c90bca
Susan Subedi <susansubedi34@gmail.com>
2020-01-17 16:57:39 -0600

    heroku fix for redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,18 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
-const middlewares = [logger];
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+const middlewares = [];
+
+let store;
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+  store = createStore(rootReducer, applyMiddleware(...middlewares));
+} else {
+  store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
+}
 export const persistor = persistStore(store);
 export default store;
